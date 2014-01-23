0fc493d9be8e93661f089cc2844ceb5fba6c8005
Stuart Nelson <stuartnelson3@gmail.com>
2014-01-24 00:23:04 +0100

    Automatically resize Graphite URLs to fit their frame

--- app/assets/javascripts/angular/controllers/frame_ctrl.js
+++ app/assets/javascripts/angular/controllers/frame_ctrl.js
@@ -10,6 +10,29 @@ angular.module("Prometheus.controllers").controller('FrameCtrl', ["$scope", "$sc
     $scope.showTab = $scope.showTab == tab ? null : tab;
   };
 
+  function createGraphiteURL() {
+    var parser = document.createElement('a');
+    parser.href = $scope.frame.url;
+    var queryStringComponents = parser.search.substring(1).split('&');
+    queryStringComponents = queryStringComponents.map(function(e) {
+      if (e.indexOf('height=') === 0 ) {
+        return setDimension(e, $scope.frameHeight().height);
+      } else if (e.indexOf('width=') === 0) {
+        var width = $scope.frameHeight().height / $scope.aspectRatio;
+        return setDimension(e, width);
+      }
+      return e;
+    });
+    parser.search = '?' + queryStringComponents.join('&') + '&decache=' + $scope.refreshCounter;
+    return parser.href;
+  }
+
+  function setDimension(dimensionKeyValue, dimensionValue) {
+    var split = dimensionKeyValue.split("=");
+    split[1] = dimensionValue;
+    return split.join("=");
+  }
+
   $scope.getTitle = function() {
     if ($scope.frame.title) {
       return VariableInterpolator($scope.frame.title, $scope.vars);
@@ -23,9 +46,17 @@ angular.module("Prometheus.controllers").controller('FrameCtrl', ["$scope", "$sc
     }
   };
 
+  $scope.frameURL = function() {
+    if ($scope.frame.graphite) {
+      return $sce.trustAsResourceUrl(createGraphiteURL());
+    } else {
+      return $sce.trustAsResourceUrl(VariableInterpolator($scope.frame.url, $scope.vars) + "?decache=" + $scope.refreshCounter);
+    }
+  };
+
   $scope.refreshFrame = function() {
     $scope.refreshCounter++;
-    $scope.frameURL = $sce.trustAsResourceUrl(VariableInterpolator($scope.frame.url, $scope.vars) + "?decache=" + $scope.refreshCounter);
+    $scope.frameURL();
   };
 
   $scope.$on('refreshDashboard', function(ev) {
