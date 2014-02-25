d0fbf007002af26b70777d1dcc3d74c1e9269bd3
Johannes 'fish' Ziemke <github@freigeist.org>
2014-02-25 20:40:20 +0100

    Log to stdout in production by default

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ PrometheusDashboard::Application.configure do
   # config.log_tags = [ :subdomain, :uuid ]
 
   # Use a different logger for distributed setups.
-  # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
+  config.logger = Logger.new(STDOUT)
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
