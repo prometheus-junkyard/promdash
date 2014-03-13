964e1a59c7a53e6c8215671353ec81ee2cb8cc27
Stuart Nelson <stuartnelson3@gmail.com>
2014-03-12 22:18:26 -0700

    add some more dashboard features

--- spec/features/dashboards_spec.rb
+++ spec/features/dashboards_spec.rb
@@ -5,4 +5,17 @@ feature "Dashboard" do
     visit root_path
     expect(page.has_content?("Dashboards")).to eq(true)
   end
+
+  scenario "visiting the dashboard new page" do
+    visit root_path
+    click_button("New Dashboard")
+    expect(page).to have_content("New dashboard")
+  end
+
+  scenario "creating the dashboard" do
+    visit new_dashboard_path
+    fill_in('Name', with: 'New Dashboard')
+    find('.actions input').click
+    expect(page).to have_content(/successfully created/)
+  end
 end
