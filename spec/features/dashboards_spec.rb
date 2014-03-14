231f6d7196b671293b22c5b618f81ea9a6c073b2
Stuart Nelson <stuartnelson3@gmail.com>
2014-03-13 21:47:04 -0700

    test dashboard edit

--- spec/features/dashboards_spec.rb
+++ spec/features/dashboards_spec.rb
@@ -17,5 +17,53 @@ feature "Dashboard" do
     fill_in('Name', with: 'New Dashboard')
     find('.actions input').click
     expect(page).to have_content(/successfully created/)
+    expect(page).to have_content(/new dashboard/i)
+    visit root_path
+    expect(page).to have_content(/new dashboard/i)
+  end
+
+  describe "editing the dashboard" do
+    before(:each) do
+      visit new_dashboard_path
+      fill_in('Name', with: 'New Dashboard')
+      find('.actions input').click
+      visit root_path
+    end
+
+    describe "the name" do
+      scenario "valid" do
+        click_link('Rename')
+        find("#dashboard_name").set("some other name")
+        click_button("Update Dashboard")
+        expect(page).to have_content(/successfully updated/)
+        expect(page).to have_content(/some other name/)
+      end
+
+      scenario "invalid" do
+        click_link('Rename')
+        find("#dashboard_name").set("")
+        click_button("Update Dashboard")
+        expect(page).to have_content(/error/)
+        expect(page).to have_content(/editing dashboard/i)
+      end
+    end
+
+    describe "the slug" do
+      scenario "valid" do
+        click_link('Rename')
+        find("#dashboard_slug").set("valid-slug-name")
+        click_button("Update Dashboard")
+        expect(page).to have_content(/successfully updated/)
+        expect(current_url).to include("valid-slug-name")
+      end
+
+      scenario "invalid" do
+        click_link('Rename')
+        find("#dashboard_slug").set("invalid slug name")
+        click_button("Update Dashboard")
+        expect(page).to have_content(/error/)
+        expect(page).to have_content(/editing dashboard/i)
+      end
+    end
   end
 end
