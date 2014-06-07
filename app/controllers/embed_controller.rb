b09a99548adadb62c19b9c4618bc80d181eb8626
Stuart Nelson <stuartnelson3@gmail.com>
2014-06-06 23:40:30 -0400

    Add CORS header to embed#show

--- app/controllers/embed_controller.rb
+++ app/controllers/embed_controller.rb
@@ -3,6 +3,7 @@ class EmbedController < ApplicationController
   before_action :set_dashboard_via_slug, only: :show
 
   def show
+    response.headers['Access-Control-Allow-Origin'] = '*'
     @servers = Server.all
     render layout: 'single_widget'
   end
