53373628397d5e6eb52890545a4c53f4caf70448
Stuart Nelson <stuartnelson3@gmail.com>
2014-10-31 12:47:08 +0100

    Auto-detect graphite widget links.

--- app/assets/javascripts/angular/controllers/dashboard_ctrl.js
+++ app/assets/javascripts/angular/controllers/dashboard_ctrl.js
@@ -142,6 +142,7 @@ angular.module("Prometheus.controllers")
     $scope.widgets.push({
       type: "frame",
       title: "",
+      graphite: /^http:\/\/graphite/.test(url),
       url: url
     });
   };
