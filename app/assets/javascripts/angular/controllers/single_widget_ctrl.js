eea3727716c4554cccf5db8432c3c93661e6a7f5
Stuart Nelson <stuartnelson3@gmail.com>
2015-02-02 12:41:25 +0100

    Add basic check that controller loads correctly.

--- app/assets/javascripts/angular/controllers/single_widget_ctrl.js
+++ app/assets/javascripts/angular/controllers/single_widget_ctrl.js
@@ -1,9 +1,9 @@
 angular.module("Prometheus.controllers").controller('SingleWidgetCtrl', ["$window", "$timeout", "$scope", "$http", "URLConfigDecoder", "GraphRefresher", "ServersByIDObject", "FullScreenAspectRatio", "ThemeManager", "SharedGraphBehavior", function($window, $timeout, $scope, $http, URLConfigDecoder, GraphRefresher, ServersByIDObject, FullScreenAspectRatio, ThemeManager, SharedGraphBehavior) {
-  var graphBlob = URLConfigDecoder(blob);
+  var graphBlob = URLConfigDecoder(window.blob);
   $scope.widget = graphBlob.widget;
   $scope.servers = servers;
   $scope.serversById = ServersByIDObject($scope.servers);
-  $scope.globalConfig = graphBlob.globalConfig;
+  $scope.globalConfig = graphBlob.globalConfig || {};
   $scope.globalConfig.aspectRatio = FullScreenAspectRatio();
   ThemeManager.setTheme($scope.globalConfig.theme);
   SharedGraphBehavior($scope);
