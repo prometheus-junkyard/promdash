4053bbf7d84dc6de42e5b777555151d0a02d012b
Stuart Nelson <stuartnelson3@gmail.com>
2015-02-02 16:18:37 +0100

    Update single widget ctrl spec.

--- spec/javascripts/angular/controllers/single_widget_ctrl_spec.js
+++ spec/javascripts/angular/controllers/single_widget_ctrl_spec.js
@@ -2,17 +2,22 @@ var servers = [];
 var graphBlob = {};
 describe('SingleWidgetCtrl', function() {
   var $controller;
-  var scope;
-  beforeEach(inject(function(_$controller_, $rootScope){
+  var $scope;
+  var themeManager;
+  beforeEach(inject(function(_$controller_, $rootScope, _ThemeManager_){
     $controller = _$controller_;
-    scope =  $rootScope;
+    $scope = $rootScope.$new(true);
+    themeManager = _ThemeManager_;
   }));
 
   it('loads correctly', function() {
-    var $scope = scope.$new(true);
-    var controller = $controller('SingleWidgetCtrl', { $scope: $scope });
+    $controller('SingleWidgetCtrl', { $scope: $scope });
     expect($scope.servers).toEqual([]);
     expect($scope.globalConfig.aspectRatio).not.toBe(undefined);
   });
 
+  it('sets a default theme', function() {
+    $controller('SingleWidgetCtrl', { $scope: $scope });
+    expect(themeManager.theme).toEqual('dark_theme');
+  });
 });
