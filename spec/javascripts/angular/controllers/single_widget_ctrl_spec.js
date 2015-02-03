8d9ceac2cb860536d6189b14788c723fea566da0
Stuart Nelson <stuartnelson3@gmail.com>
2015-02-03 13:52:42 +0100

    Update SharedGraphBehavior to accept config object instead of squashing with global value or default

--- spec/javascripts/angular/controllers/single_widget_ctrl_spec.js
+++ spec/javascripts/angular/controllers/single_widget_ctrl_spec.js
@@ -4,20 +4,28 @@ describe('SingleWidgetCtrl', function() {
   var $controller;
   var $scope;
   var themeManager;
-  beforeEach(inject(function(_$controller_, $rootScope, _ThemeManager_){
+  var fullscreenAspectRatio;
+  beforeEach(inject(function(_$controller_, $rootScope, _ThemeManager_, _FullScreenAspectRatio_){
     $controller = _$controller_;
     $scope = $rootScope.$new(true);
     themeManager = _ThemeManager_;
+    fullscreenAspectRatio = _FullScreenAspectRatio_;
   }));
 
-  it('loads correctly', function() {
+  beforeEach(function() {
     $controller('SingleWidgetCtrl', { $scope: $scope });
+  });
+
+  it('loads correctly', function() {
     expect($scope.servers).toEqual([]);
-    expect($scope.globalConfig.aspectRatio).not.toBe(undefined);
+  });
+
+  it('sets the aspect ratio to that of the full screen', function() {
+    var r = fullscreenAspectRatio();
+    expect($scope.globalConfig.aspectRatio).toEqual(r);
   });
 
   it('sets a default theme', function() {
-    $controller('SingleWidgetCtrl', { $scope: $scope });
     expect(themeManager.theme).toEqual('dark_theme');
   });
 });
