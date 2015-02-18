64c6a153d7a310e9c453bcb6353042323d4ec9b5
Stuart Nelson <stuartnelson3@gmail.com>
2015-02-18 09:37:53 +0100

    Add X's to close open menus, and close menus when clicking on body.

--- app/assets/javascripts/angular/services/widget_tab_service.js
+++ app/assets/javascripts/angular/services/widget_tab_service.js
@@ -2,10 +2,21 @@ angular.module("Prometheus.services").factory('WidgetTabService', ["$timeout", "
   return function(scope) {
     scope.toggleTab = function(ev, tab) {
       scope.showTab = scope.showTab == tab ? null : tab;
+      $(ev.currentTarget).closest('.graph_config_menu').find('.btn_active').removeClass('btn_active');
+      if (scope.showTab) {
+        $(ev.currentTarget).addClass('btn_active');
+      }
       $timeout(CheckWidgetMenuAlignment(ev.currentTarget, tab), 0);
     };
 
+    scope.closeTab = function(ev) {
+      var w = $(ev.currentTarget).closest('.graph_control_tabbar').siblings('.js_widget_wrapper');
+      w.find('.btn_active').removeClass('btn_active');
+      scope.showTab = null;
+    };
+
     scope.$on('closeTabs', function() {
+      $('.btn_active').removeClass('btn_active');
       scope.showTab = null;
     });
   };
