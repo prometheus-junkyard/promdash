f0536555ca6afb65630488681a4f30337f00938e
Stuart Nelson <stuartnelson3@gmail.com>
2015-02-18 09:44:21 +0100

    Remove errant local variables

--- spec/javascripts/angular/services/url_generator_spec.js
+++ spec/javascripts/angular/services/url_generator_spec.js
@@ -8,7 +8,6 @@ describe('URLGenerator', function() {
   it('allows urls with custom paths, no trailing slash', function() {
     ['http://promdash.server.com/prometheus', 'http://promdash.com'].forEach(function(s) {
       ['/api/query_range', '/api/query', '/api/metrics', '/arbitrary/endpoint'].forEach(function(ep) {
-        var s = 'http://promdash.server.com/prometheus';
         var url = urlGenerator(s, ep);
         expect(url).toEqual(s + ep);
       });
@@ -18,7 +17,6 @@ describe('URLGenerator', function() {
   it('allows urls with custom paths, with trailing slash', function() {
     ['http://promdash.server.com/prometheus/', 'http://promdash.com/'].forEach(function(s) {
       ['/api/query_range', '/api/query', '/api/metrics', '/arbitrary/endpoint'].forEach(function(ep) {
-        var s = 'http://promdash.server.com/prometheus/';
         var url = urlGenerator(s, ep);
         expect(url).toEqual(s.substring(0, s.length - 1) + ep);
       });
