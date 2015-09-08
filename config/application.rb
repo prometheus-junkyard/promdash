63f7066e700ebb2ac4083403295d941e045cde31
Mat Davies <mat@improbable.io>
2015-09-08 15:13:02 +0100

    bootstrap 3.3.5 update + correcting missing overrides fix icons in prod

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module PrometheusDashboard
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
+    config.assets.precompile << /\.(?:svg|eot|woff|woff2|ttf)$/
     config.assets.precompile << 'base.js'
 
     # Required for working dashboard JSON PUTs.
