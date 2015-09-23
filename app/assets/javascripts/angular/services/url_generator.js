db0ad72c0a1523a83fad03ec494fc42a5c68fa66
Stuart Nelson <stuartnelson3@gmail.com>
2015-09-23 15:08:40 +0200

    correctly connect prometheus url and path in url generator
    fixes #450

--- app/assets/javascripts/angular/services/url_generator.js
+++ app/assets/javascripts/angular/services/url_generator.js
@@ -3,6 +3,11 @@ angular.module("Prometheus.services").factory('URLGenerator', ["VariableInterpol
     url = VariableInterpolator(url, vars);
     var a = document.createElement('a');
     a.href = url;
+
+    if (path && path[0] !== "/") {
+      path = "/" + path;
+    }
+
     a.pathname = a.pathname.replace(/\/?$/, path);
     return a.href;
   };
