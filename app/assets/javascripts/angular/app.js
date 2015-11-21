880f26f4775170d318f1c2cf871e14c5a7165fb8
stuart nelson <stuartnelson3@gmail.com>
2015-11-21 11:03:44 +0100

    enable $httpProvider.useApplyAsync
    processes multiple received http requests in the
    same digest cycle, hopefully to improve
    performance (e.g. during dashboard load)
    https://docs.angularjs.org/api/ng/provider/$httpProvider#useApplyAsync

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -19,6 +19,7 @@ angular.module("Prometheus",
   if ($location.hash()[0] === "?") {
     $location.url($location.path() + $location.hash());
   }
-}]).config(['$locationProvider', function($locationProvider) {
+}]).config(['$locationProvider', '$httpProvider', function($locationProvider, $httpProvider) {
+  $httpProvider.useApplyAsync(true);
   $locationProvider.html5Mode(true);
 }]);
